c36b49cbfeb3bab1f345c6fd0793bfc86c575ac3
2113angel <delacruzangelgrace10@gmail.com>
2025-08-04 12:50:19 +0800

    Activity 1 and 2 : Understanding the Dart

---
+++ Act1.dart
@@ -0,0 +1,10 @@
+ void main() {
+   var greetings = "Welcome";
+   var name = "Alice";
+   print("Welcome, Alice!");
+
+  dynamic myVariable = 25;
+  myVariable = "Welcome 25";
+  print(myVariable);
+ }
+
